9917b70c1d21ca6f85956a2728638710affceacb
nascvi <vaiiho.nascimbene@gmail.com>
2021-11-10 14:51:44 +0100

    intial bruegel v2 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const card = [
+  {"src": "/img/"},
+  {"src": "/img/"},
+  {"src": "/img/"},
+  {"src": "/img/"},
+  {"src": "/img/"},
+  {"src": "/img/"},
+  {"src": "/img/"},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1 className="title">
+        Jouer avec Bruegel - Le Memory
+      </h1>
+      <br /><br />
+      <h2 className="title">
+        <i>
+          <p>Cet ouvrage de l'Elève aux Maîtres et l'Artiste, est un hômage au personnes que nous avons perdu</p>
+        Que Renaisse un jour l'Italie, coeur de l'art, de culture, de spiritualité, et des jeux de l'enfance, ad vitam aeternum. Amen ✝
+        </i>
+      </h2>
+      <div className="game container-md">
+
+        <img src="../../../images/jouerAvecBruegel.jpg" />
+        <div className="cards-container">
+          {cards.map(card => <Card onClick={() => onCardClick(card)} key={card.id} {...card} />)}
+        </div>
+      </div> */}
+      <div className="copy">
+        ©2020 <a className="svsem" href="https://svsem.github.io/Memorai/">Svyatoslav Semelov</a> - Aldo Nascimbene @6ème Evans - W.R - <a href="https://github.com/landscapersParis">Vaiiho Nascimbene(lpChamps`)</a>
+      </div>
     </div>
   );
 }
